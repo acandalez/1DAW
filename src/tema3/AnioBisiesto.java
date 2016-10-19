9e20c78f3f8e697363538724dea13406b58c3e78
Alexander Acanda <acand@DESKTOP-ERH4GM9>
2016-10-19 12:19:20 +0200

    1ºDAW

---
+++ src/tema3/AnioBisiesto.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema3;
+
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author acand
+ */
+public class AnioBisiesto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int iAnio=0; 
+       Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un año (formato 0000) : ");
+        iAnio = sc.nextInt();
+        
+        //Forma con formula
+        //Si año / 4 es igual a 0 y (año / 100 es distinto de 0 Ó año entre 400 es igual a 0)
+        if ((iAnio % 4 == 0) && ((iAnio % 100 != 0) || (iAnio % 400 == 0))){
+	System.out.println("El año es bisiesto");
+        }else{
+	System.out.println("El año no es bisiesto");
+        }
+        
+       /* 
+        //Usando la clase GregorianCalendar y el metodo isLeapYear.
+        GregorianCalendar calendario = new GregorianCalendar();
+        
+ 
+        if (calendario.isLeapYear(iAnio)){
+                System.out.println("El año es bisiesto");
+        }else{
+                System.out.println("El año no es bisiesto");
+    }*/
+    }
+}
--- src/tema3/DiaMes.java
+++ src/tema3/DiaMes.java
@@ -24,7 +24,7 @@ public class DiaMes {
         System.out.print("Seleccione un mes del año por medio de valor "
                 + "numerico del 1 al 12: ");
         seleccion = sc.nextInt();
-        if(seleccion<=12){
+        if(seleccion<12 || seleccion <= 1 ){
         if(seleccion==1){ System.out.println("Enero tiene 31 dias"); }
         if(seleccion==2){ System.out.println("Febrero tiene 28 dias"); }
         if(seleccion==3){ System.out.println("Marzo tiene 31 dias"); }
@@ -39,5 +39,47 @@ public class DiaMes {
         if(seleccion==12){ System.out.println("Diciembre tiene 31 dias"); }
         }else{System.out.println("El mes seleccionado no existe");}
     }   
-    
+    /*
+     Scanner sc = new Scanner(System.in);
+        int mes;
+        System.out.print("Introduzca número de mes: ");
+        mes =  sc.nextInt();
+        if(mes < 1 || mes > 12) //se comprueba que el valor del mes es correcto
+            System.out.println("Mes incorrecto");
+        else{  //si el mes es correcto
+            switch(mes){  //se muestra el nombre mediante una instrucción switch
+                case 1: System.out.print("Enero");
+                        break;
+                case 2: System.out.print("Febrero");
+                        break;
+                case 3: System.out.print("Marzo");
+                        break;
+                case 4: System.out.print("Abril");
+                        break;
+                case 5: System.out.print("Mayo");
+                        break;
+                case 6: System.out.print("Junio");
+                        break;
+                case 7: System.out.print("Julio");
+                        break;
+                case 8: System.out.print("Agosto");
+                        break;
+                case 9: System.out.print("Septiembre");
+                        break;
+                case 10: System.out.print("Octubre");
+                        break;
+                case 11: System.out.print("Noviembre");
+                        break;
+                case 12: System.out.print("Diciembre");
+                        break;
+            }
+            // mostrar si es un mes de 30, 31 0 28 días
+            if(mes == 4 || mes == 6 || mes == 9 || mes == 11)       
+               System.out.println(" es un mes de 30 días");
+            else if(mes == 2)
+                     System.out.println(" es un mes de 28 días");
+                   else
+                      System.out.println(" es un mes de 31 días");
+        }
+    */
 }
